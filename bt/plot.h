18d29b8a1e6e06411e62a625e091ec2e86b8d6f7
稻草人 <84495086+dybeta2021@users.noreply.github.com>
2023-05-16 13:52:13 +0800

    Update plot.h

--- bt/plot.h
+++ bt/plot.h
@@ -30,20 +30,6 @@ namespace bt::plot {
         plot2.ylabel("price");
         plot2.drawCurve(idx, price).label("price");
 
-        for (size_t i = 0; i < order_volume.size(); i++) {
-            if (order_volume[i] > 0) {
-                const std::string sh = fmt::format("set arrow from graph {0},{1} to graph {0},{2} filled back lw 3 lc rgb \"red\"", idx[i], price[i]*0.99, price[i]);
-                plot2.gnuplot(sh);
-            }
-            // If the order_volume is negative, draw a downward green arrow at the corresponding price
-            else if (order_volume[i] < 0) {
-                const std::string sh = fmt::format("set arrow from graph {0},{1} to graph {0},{2} filled back lw 3 lc rgb \"red\"", idx[i], price[i], price[i]*0.99);
-                plot2.gnuplot(sh);
-            }
-        }
-
-
-
         // Create figure to hold plot
         Figure fig = {{plot1}, {plot2}};
         // Create canvas to hold figure
