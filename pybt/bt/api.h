1e2d3c1d865f56e8825d5da160eaf36f56f93430
稻草人 <84495086+dybeta2021@users.noreply.github.com>
2023-06-28 11:24:33 +0800

    add pybt

---
+++ pybt/bt/api.h
@@ -0,0 +1,131 @@
+#pragma once
+#include "fmt/format.h"
+
+#include "sqlite_orm/sqlite_orm.h"
+#include <string>
+#include <vector>
+
+#include "data.h"
+namespace bt::api {
+    using bt::data::Bar;
+    using bt::data::Order;
+    using bt::data::Position;
+
+    class DataApi {
+    private:
+        std::vector<Bar> data_{};
+        size_t size_ = 0;
+
+    public:
+        explicit DataApi(const std::string &db_path, const std::string &table_name = "quote") {
+            using namespace sqlite_orm;
+            auto storage = make_storage(db_path,
+                                        make_table(table_name,
+                                                   make_column("index", &Bar::idx),
+                                                   make_column("datetime", &Bar::datetime),
+                                                   make_column("open", &Bar::open_price),
+                                                   make_column("high", &Bar::high_price),
+                                                   make_column("low", &Bar::low_price),
+                                                   make_column("close", &Bar::close_price)));
+//            storage.sync_schema(true);
+            data_ = storage.get_all<Bar>();
+            size_ = data_.size();
+            SPDLOG_INFO("Data size:{}, start:{}, end:{}.", data_.size(), data_[0].datetime, data_[data_.size() - 1].datetime);
+        }
+
+        [[nodiscard]] auto get_size() const {
+            return size_;
+        }
+
+        auto get_bar(const size_t &idx) {
+            if (idx < size_) {
+                return data_[idx];
+            } else {
+                SPDLOG_ERROR("idx range:{},{} error:{}", 0, size_, idx);
+                throw std::invalid_argument("received error idx");
+            }
+        }
+    };
+
+    class RecordOrder {
+    private:
+        std::vector<Order> store_{};
+
+    public:
+        void insert_record(const Order &order) {
+            store_.push_back(order);
+        }
+
+        void clear() {
+            store_.clear();
+        }
+
+        void save(const std::string &db_path, const std::string &table_name = "order") {
+            using namespace sqlite_orm;
+
+            auto table = make_table(table_name,
+                                    make_column("type", &Order::type),
+                                    make_column("datetime", &Order::datetime),
+                                    make_column("price", &Order::price),
+                                    make_column("volume", &Order::volume));
+
+            auto storage = make_storage(db_path, table);
+            storage.sync_schema();
+            storage.remove_all<Order>();
+            // 分批次写入
+            const int chunksize = 5000; // 每次插入的数据量
+            int n = store_.size(); // 总的数据量
+            for (int i = 0; i < n; i += chunksize) {
+                int j = std::min(i + chunksize, n); // 下一批数据的结束位置
+                auto begin = store_.begin() + i; // 当前批次数据的开始迭代器
+                auto end = store_.begin() + j; // 当前批次数据的结束迭代器
+                storage.replace_range(begin, end); // 使用 replace_range 方法插入数据
+            }
+        }
+    };
+
+
+    class RecordPosition {
+    private:
+        std::vector<Position> store_{};
+
+    public:
+        void insert_record(const Position &pos) {
+            store_.push_back(pos);
+        }
+
+        void clear() {
+            store_.clear();
+        }
+
+        void save(const std::string &db_path, const std::string &table_name = "position") {
+            using namespace sqlite_orm;
+
+            auto table = make_table(table_name,
+                                    make_column("status", &Position::status),
+                                    make_column("datetime", &Position::datetime),
+                                    make_column("average_price", &Position::average_price),
+                                    make_column("current_price", &Position::current_price),
+                                    make_column("volume", &Position::volume),
+                                    make_column("floating_pnl", &Position::floating_pnl),
+                                    make_column("closing_pnl", &Position::closing_pnl),
+                                    make_column("realized_pnl", &Position::realized_pnl),
+                                    make_column("total_pnl", &Position::total_pnl));
+
+            auto storage = make_storage(db_path, table);
+            storage.sync_schema();
+            storage.remove_all<Position>();
+            // 分批次写入
+            const int chunksize = 5000; // 每次插入的数据量
+            int n = store_.size(); // 总的数据量
+            for (int i = 0; i < n; i += chunksize) {
+                int j = std::min(i + chunksize, n); // 下一批数据的结束位置
+                auto begin = store_.begin() + i; // 当前批次数据的开始迭代器
+                auto end = store_.begin() + j; // 当前批次数据的结束迭代器
+                storage.replace_range(begin, end); // 使用 replace_range 方法插入数据
+            }
+        }
+
+    };
+
+}// namespace bt::api
